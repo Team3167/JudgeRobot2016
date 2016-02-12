d006d20a052a6c6214e74324a367b056b85e9907
Shop Laptop <none>
2016-02-11 19:13:27 -0500

    Copied from Liam's project

--- src/frc/team3167/util/Conversions.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team3167.util;
-
-public class Conversions
-{
-	// The convention used here is "multiply by this to get the named conversion."
-	// For inverse conversion, divide.
-	public static final double RPMToRadPerSec = 0.104720;
-	public static final double RadPerSecToRPM = 1 / RPMToRadPerSec;
-}
--- src/org/usfirst/frc/team3167/robot/Robot.java
+++ src/org/usfirst/frc/team3167/robot/Robot.java
@@ -1,16 +1,14 @@
 
 package org.usfirst.frc.team3167.robot;
 
-import org.usfirst.frc.team3167.util.Conversions;
-import org.usfirst.frc.team3167.util.JoystickButton;
-
-import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.vision.AxisCamera;
+import edu.wpi.first.wpilibj.Talon;
+//import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,29 +17,12 @@ import edu.wpi.first.wpilibj.vision.AxisCamera;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot 
-{
-	private Joystick driveJoystick1 = new Joystick(1);
-	private Joystick driveJoystick0 = new Joystick(0);
-	  
-	Preferences prefs = Preferences.getInstance();
-
-    private static final double pullBallInSpeed = 0.5;
-    private static final double shootBallOutSpeed = 1.0;
-
-    private Jaguar leftBallWheelMotor;
-    private Jaguar rightBallWheelMotor;
-    private Joystick driveStick;
-
-    private Jaguar leftMotorA;
-    private Jaguar leftMotorB;
-
-    private Jaguar rightMotorA;
-    private Jaguar rightMotorB;
-    private Joystick driveStick;
-    private RobotDrive driveA;
-    private RobotDrive driveB;
-
+public class Robot extends IterativeRobot {
+    /*final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;*/
+   
     private static final int leftStickUpDown = 2;// positive down
     private static final int leftStickLeftRight = 1;// positive right
     private static final int rightStickUpDown = 5;// positive down
@@ -56,136 +37,114 @@ public class Robot extends IterativeRobot
     private static final int bottomTrigger = 3;// On/off; positive left
 
     private static final double turnRateScale = 0.8;
+    
+    private static final double pullBallInSpeed = 0.5;
+    private static final double shootBallOutSpeed = 1.0;
+    
+    private Joystick driveStick; 
+    
+    private Talon leftMotorA;
+    private Talon leftMotorB;
+    
+    private Talon rightMotorA; 
+    private Talon rightMotorB;
+    
+    private Jaguar leftBallWheelMotor; 
+    private Jaguar rightBallWheelMotor; 
+    
+    private RobotDrive driveA; 
+    private RobotDrive driveB;
 	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() 
-    {
-        driveStick = new Joystick(1);
-        leftBallWheelMotor = new Jaguar(1, 1);  //public Jaguar(int slot, int channel)
-        rightBallWheelMotor = new Jaguar(1, 2);
-
-        leftMotorA = new Jaguar(1, 3);
-        leftMotorB = new Jaguar(1, 4);
-
-        rightMotorA = new Jaguar(1, 5);
-        rightMotorB = new Jaguar(1, 6);
-
-        driveStick = new Joystick(1);
-
-        driveA = new RobotDrive(leftMotorA, rightMotorA);
-        driveB = new RobotDrive(leftMotorB, rightMotorB);
-
-        driveA.arcadeDrive(-driveStick.getRawAxis(leftStickUpDown),
-                -driveStick.getRawAxis(leftStickLeftRight) * turnRateScale);
-        driveB.arcadeDrive(-driveStick.getRawAxis(leftStickUpDown),
-                -driveStick.getRawAxis(leftStickLeftRight) * turnRateScale);
-
-    	//prefs.putDouble("Kp", 1.0);
-    	//prefs.putDouble("Ki", 0.0);
+    public void robotInit() {
+        /*chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);*/
+        
+        driveStick = new Joystick(1); 
+        
+    	leftMotorA = new Talon(0); 
+        leftMotorB = new Talon(1); 
+        
+        rightMotorA = new Talon(2); 
+        rightMotorB = new Talon(3); 
+        
+        leftBallWheelMotor = new Jaguar(4); 
+        rightBallWheelMotor = new Jaguar(5); 
+        
+        driveA = new RobotDrive(leftMotorA, rightMotorA); 
+        driveB = new RobotDrive(leftMotorB, rightMotorB); 
     }
     
-    private void DoCommonUpdates()
-    {
-    	updateSmartDashboard();
-    	
-    	// CAN objects - this line sends the commands
-    	CANJaguar.updateSyncGroup(RobotConfiguration.wheelCANSyncGroup);
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+    	/*autoSelected = (String) chooser.getSelected();
+//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);*/
     }
-    
-    public void updateSmartDashboard()
-    {
-    	//SmartDashboard.putNumber("Front Right Wheel Angle", drive.GetWheel(1).GetWheelAngle());
-    	//SmartDashboard.putNumber("Rear Left Wheel Angle", drive.GetWheel(2).GetWheelAngle());
-    	//SmartDashboard.putNumber("Rear Right Wheel Angle", drive.GetWheel(3).GetWheelAngle());
-    	
-    	//SmartDashboard.putNumber("Left Front Wheel KP", drive.GetWheel(0).getKP());
-    	//SmartDashboard.putNumber("Left Front Wheel KI", drive.GetWheel(0).getKI()); 
-    	//SmartDashboard.putNumber("Left Front Wheel KD", drive.GetWheel(0).getKD()); 
-    	//SmartDashboard.putNumber("testSig", testSig); 
-    	
-    	//SmartDashboard.putNumber("Left Front Wheel speed", drive.GetWheel(0).GetWheelVelocity());
-    	//SmartDashboard.putNumber("Left Front Wheel command speed", drive.GetWheel(0).getCANMotor().get() / drive.GetWheel(0).getGearRatio() * Conversions.RPMToRadPerSec);
-    }
-    
-    public void autonomousInit()
-    {
-    }
-    
+
     /**
      * This function is called periodically during autonomous
      */
-    public void autonomousPeriodic()
-    {
-    	DoCommonUpdates();
-    }
-    
-    public void teleopInit()
-    {
-    }
-    
-    /**
-     * Function called as fast as possible during operator control.
-     * Handles button presses only.
-     */
-    public void teleopContinuous()
-    {
-    }
+    public void autonomousPeriodic() {
+    	/*switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here   
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+            break;*/
+    	}
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() 
-    {
-    	DoCommonUpdates();
+    public void teleopPeriodic() {
+    	
+    	//Can be modularized to use 1 class for controlling shooter.
+    	//Class is created, called "BallWheels" 
     	
     	if(driveStick.getRawButton(5))// Suck ball in
         {
-            leftBallWheelMotor.set(pullBallInSpeed);
+        	leftBallWheelMotor.set(pullBallInSpeed);
             rightBallWheelMotor.set(-pullBallInSpeed);
         }
         else if(driveStick.getRawButton(3))// Shoot ball out
         {
-            leftBallWheelMotor.set(-shootBallOutSpeed);
+        	leftBallWheelMotor.set(-shootBallOutSpeed);
             rightBallWheelMotor.set(shootBallOutSpeed);
         }
         else
         {
-            leftBallWheelMotor.set(0.0);
+        	leftBallWheelMotor.set(0.0);
             rightBallWheelMotor.set(0.0);
         }
-    }
-    
+    	
+    	driveA.arcadeDrive(-driveStick.getY(), -driveStick.getTwist() * turnRateScale);
+        driveB.arcadeDrive(-driveStick.getY(), -driveStick.getTwist() * turnRateScale);
+    	
+    	//driveA.arcadeDrive(driveStick.getTwist(), -driveStick.getY());
+        //driveB.arcadeDrive(driveStick.getTwist(), -driveStick.getY());
+        
+    }    
     /**
      * This function is called periodically during test mode
      */
-    public void testPeriodic()
-    {
-    }
+    public void testPeriodic() {
     
-    public void disabledInit()
-    {
-    	// Call test print statements to execute only one time
-    	//drive.PrintTest();
     }
-    
-    public void disabledPeriodic()
-    {
-    	CANJaguar.updateSyncGroup(RobotConfiguration.wheelCANSyncGroup);
-        identifyControllerButtons();
-    }
-
-    private void identifyControllerButtons()
-    {
-        int i;
-        for (i = 0; i < 12; i++)
-        {
-            if (driveStick.getRawAxis(i) > 0.5)
-                System.out.println(i + " -> Positive");
-            else if (driveStick.getRawAxis(i) < -0.5)
-                System.out.println(i + " -> Negative");
-        }
-    }
+   
 }
