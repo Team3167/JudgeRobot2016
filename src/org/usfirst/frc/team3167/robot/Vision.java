82ce9ae69597aadfa14bc04920ca894dd80e244d
Shop Laptop <none>
2016-02-16 15:20:54 -0500

    Modularized camera code

---
+++ src/org/usfirst/frc/team3167/robot/Vision.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team3167.robot;
+
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class Vision {
+	
+	public void enable() 
+	{
+		CameraServer camServer = CameraServer.getInstance();  
+		
+		camServer.setQuality(25); 
+		camServer.setSize(75); 
+		
+		camServer.startAutomaticCapture("cam0");
+	}
+}
