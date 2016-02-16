23517975e7da600fe6a8194c1d44d4d60ee76b1e
Shop Laptop <none>
2016-02-16 16:10:57 -0500

    Added configuration class and updated Vision class.

---
+++ src/org/usfirst/frc/team3167/robot/RobotConfiguration.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team3167.robot;
+
+/*
+ * Configuartion for xbox controller axis and button input values.
+ * None of these are relevant for the current Joystick we are
+ * using. They're just left here for any last minute changes
+ * we might make regarding control of the robot. 
+ */
+public class RobotConfiguration {
+    
+	private static final int leftStickUpDown = 2;// positive down
+    private static final int leftStickLeftRight = 1;// positive right
+    private static final int rightStickUpDown = 5;// positive down
+    private static final int rightStickLeftRight = 4;// positive right
+    //private static final int directionalPadUpDown = ;
+    private static final int directionalPadLeftRight = 6;// On/off; positive right
+    //private static final int buttonA = ;// On/off
+    //private static final int buttonB = ;// On/off
+    //private static final int buttonX = ;// On/off
+    //private static final int buttonY = ;// On/off
+    //private static final int topTrigger = ;// On/off
+    private static final int bottomTrigger = 3;// On/off; positive left
+}
