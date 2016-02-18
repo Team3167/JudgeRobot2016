ae4b4448402ced6718e9e41816bb0b215ec77f6f
Shop Laptop <none>
2016-02-18 17:40:33 -0500

    Updated RobotConfiguration. Made Changes to Autonomous

--- src/org/usfirst/frc/team3167/autonomous/Autonomous.java
+++ src/org/usfirst/frc/team3167/autonomous/Autonomous.java
@@ -1,15 +1,12 @@
 package org.usfirst.frc.team3167.autonomous;
 
-import org.usfirst.frc.team3167.drive.QuadArcadeDrive;
 import org.usfirst.frc.team3167.robot.RobotConfiguration;
 
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Autonomous {
-		
-	private RobotConfiguration config = new RobotConfiguration(); 
-	
+			
 	private static final double halfSpeed = 0.5; 
 	private static final double fullSpeed = 1.0;  
 	
@@ -21,11 +18,11 @@ public class Autonomous {
 		
 		//TODO: Find which motors to spin in reverse and which ones to spin regularly
 		//TODO: Check if all motors spin at same rate, if not, adjust in code
-		config.leftMotorA.set(fullSpeed);
-		config.leftMotorB.set(fullSpeed);
+		RobotConfiguration.leftMotorA.set(fullSpeed);
+		RobotConfiguration.leftMotorB.set(fullSpeed);
 		
-		config.rightMotorA.set(-fullSpeed);
-		config.rightMotorB.set(-fullSpeed);
+		RobotConfiguration.rightMotorA.set(-fullSpeed);
+		RobotConfiguration.rightMotorB.set(-fullSpeed);
 		
 		//TODO: Find how long we want the robot to drive forward
 		timer.delay(7);	
